8213c538ed9560fc69fd1307a27471202d586953
nisanurtuz24-tech <nisanurtuz24@gmail.com>
2026-07-18 14:31:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import os
 import discord
+from discord import app_commands
 
 intents = discord.Intents.default()
 
-client = discord.Client(intents=intents)
+class MyClient(discord.Client):
+    def __init__(self):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        await self.tree.sync()
+
+client = MyClient()
 
 @client.event
 async def on_ready():
     print(f'Giriş yapıldı: {client.user}')
+
+@client.tree.command(name="love", description="Berat için özel mesaj ❤️")
+async def love(interaction: discord.Interaction): embed = discord.Embed(
+    title="💌 NisaKalpBerat'tan Mesaj",
+    description="💖 **Nisa ve Berat birbirini çok seviyor.**\n\n🤍 Bu bot, sevginizi hatırlatmak için hazırlandı.",
+    color=0xff69b4
+)
+    
+    )
+
+    await interaction.response.send_message(embed=embed)
 
 token = os.getenv("DISCORD_TOKEN")
 
