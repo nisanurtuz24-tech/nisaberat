a1ef594a2a6e77a5f3e784c573895617a53b461b
nisanurtuz24-tech <nisanurtuz24@gmail.com>
2026-07-20 16:26:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -16,6 +16,39 @@ class MyClient(discord.Client):
 
 client = MyClient()
 
+playlist = [
+    {
+        "song": "Herkesim",
+        "artist": "Aqtaii",
+        "gif": "https://tenor.com/tr/view/hello-kitty-dear-daniel-kiss-cute-gif-15829826"
+    },
+    {
+        "song": "Bize Has",
+        "artist": "Sibel Can",
+        "gif": "https://tenor.com/tr/view/my-little-pony-twilight-sparkles-pinkie-pie-kiss-gif-14364376922814963375"
+    },
+    {
+        "song": "Aşkım",
+        "artist": "Bengü",
+        "gif": "https://tenor.com/tr/view/kiss-kissing-lesbian-anime-strawberry-panic-gif-9155305389744741940"
+    },
+    {
+        "song": "Dinle Beni Bi'",
+        "artist": "Yüzyüzeyken Konuşuruz",
+        "gif": "https://tenor.com/tr/view/renako-mai-kiss-kisses-freak-out-gif-13220785228267010349"
+    },
+    {
+        "song": "Him & I",
+        "artist": "G-Eazy & Halsey",
+        "gif": "https://tenor.com/tr/view/lesbian-gif-22079307"
+    },
+    {
+        "song": "Eskisi Gibi",
+        "artist": "Lil Zey & Kozmos",
+        "gif": "https://tenor.com/tr/view/ck006-aladdin-disney-toon-disney-jasmine-gif-8200920"
+    }
+]
+
 @client.event
 async def on_ready():
     print(f"Giriş yapıldı: {client.user}")
@@ -81,6 +114,23 @@ async def baby(interaction: discord.Interaction):
         color=0xff69b4
     )
     await interaction.response.send_message(embed=embed)
+@client.tree.command(name="playlist", description="🎧 Size özel bir şarkı önerir.")
+async def playlist(interaction: discord.Interaction):
+    music = random.choice(playlist)
 
+    embed = discord.Embed(
+        title="🎧 Our Playlist",
+        color=0xff69b4
+    )
+
+    embed.add_field(
+        name="Now Playing",
+        value=f"🎵 **{music['song']}**\n👤 *{music['artist']}*",
+        inline=False
+    )
+
+    embed.set_image(url=music["gif"])
+
+    await interaction.response.send_message(embed=embed)
 token = os.getenv("DISCORD_TOKEN")
 client.run(token)
